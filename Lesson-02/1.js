52c8d33f7bc33b150ecb7dfd28e469e6ee5604d8
Alex Andriitsev <AlexAndritsev83@users.noreply.github.com>
2020-05-18 18:26:38 +0300

    Add files via upload

---
+++ Lesson-02/1.js
@@ -0,0 +1,31 @@
+// Перепишите код, заменив оператор 'if' на тернарный оператор '?'
+
+////////////////// Задание //////////////////
+// const a = 2;
+// const b = 1;
+// let result = null;
+
+// if (a + b < 4) {
+//     result = true;
+// } else {
+//     result = false;
+// }
+
+// console.log(result);
+
+////////////////// Решение //////////////////
+const a = 2;
+const b = 1;
+let result = null;
+
+result = (a + b <4) ? true : false;
+console.log(`The result is: ${result}`);
+
+
+////////////////// Решение 2 //////////////////
+const c = 2;
+const d = 1;
+let result2 = null;
+
+result2 = (c + d <4);
+console.log(`The result is: ${result2}`);
