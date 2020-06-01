d8e573d441930807a62334102e7ea27481fb76e9
Alex Andriitsev <AlexAndritsev83@users.noreply.github.com>
2020-06-01 18:28:46 +0300

    Домашняя работа по уроку 6.
    Домашняя работа по уроку 6.

---
+++ Lesson-06/2.js
@@ -0,0 +1,48 @@
+/**
+ * Задача 2.
+ *
+ * Вручную создать имплементацию функции `filter`.
+ * Логика работы ручной имплементации должна быть такой-же,
+ * как и у встроенного метода.
+ *
+ * Заметки:
+ * - Встроенный метод Array.prototype.filter использовать запрещено.
+ *
+ * Генерировать ошибки, если:
+ * - В качестве первого аргумента был передан не массив;
+ * - В качестве второго аргумента была передана не функция.
+ *
+ * Заметки:
+ * - Второй аргумент встроенного метода filter (thisArg) имплементировать не нужно.
+ */
+
+const array = ['Доброе утро!', 'Добрый вечер!', 3, 512, '#', 'До свидания!'];
+
+// Решение
+
+const filter = function(arr, callback) {
+
+    if (!Array.isArray(arguments[0])) {return null};
+    if (!(typeof(arguments[1]) === 'function')) {return null};
+
+    let  length = arr.length;
+    let  results = [];
+    for (let i = 0; i < length; i++) {
+        if (callback(arr[i], i, arr)) {
+            results.push(arr[i]);
+        }
+    }
+    return results;
+};
+
+
+//Проверка
+const filteredArray = filter(array, (element, index, arrayRef) => {
+    console.log(`${index}:`, element, arrayRef);
+
+    return element === 'Добрый вечер!';
+});
+
+console.log(filteredArray); // ['Добрый вечер!']
+
+exports.filter = filter;
---
+++ Lesson-06/3.js
@@ -0,0 +1,47 @@
+/**
+ * Задача 3.
+ *
+ * Вручную создать имплементацию функции `every`.
+ * Логика работы ручной имплементации должна быть такой-же,
+ * как и у встроенного метода.
+ *
+ * Заметки:
+ * - Встроенный метод Array.prototype.every использовать запрещено.
+ *
+ * Генерировать ошибки, если:
+ * - В качестве первого аргумента был передан не массив;
+ * - В качестве второго аргумента была передана не функция.
+ *
+ * Заметки:
+ * - Второй аргумент встроенного метода every (thisArg) имплементировать не нужно.
+ */
+
+const array = [1, 2, 3, 4, 5, 6];
+
+// Решение
+const every = function(arr, callback) {
+
+    if (!Array.isArray(arguments[0])) {return null};
+    if (!(typeof(arguments[1]) === 'function')) {return null};
+
+    let  length = arr.length;
+    
+    for (let i = 0; i < length; i++) {
+        if (!(callback(arr[i], i, arr))) {
+            return false;
+        }
+    }
+    return true;
+};
+
+
+//Проверка
+const result = every(array, (element, index, arrayRef) => {
+    console.log(`${index}:`, element, arrayRef);
+
+    return typeof element === 'number';
+});
+
+console.log(result); // true
+
+exports.every = every;
